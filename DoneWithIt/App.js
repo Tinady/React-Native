2ffdf21f72529ba166ecfbefec5b2e174af9b6dc
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-30 00:05:15 +0300

    u

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-    <Text className='text-red-800'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-800'> hello Open up App.js to start working on your app!</Text>
     <StatusBar style="auto" />
   </View>
   );
